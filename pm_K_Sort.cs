87199a4b3a2e108ae7d0a3ded49468875d99dded
jim <ows456@naver.com>
2020-12-21 09:59:33 +0900

    clone

---
+++ pm_K_Sort.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+
+public class Solution {
+    public int[] solution(int[] array, int[,] commands)
+    {
+        int[] answer = new int[] {};
+        int[] Temp_Array = new int[] {};
+        int Temp_count = 0;
+        
+        for(int i=0; i<commands.GetLength(0); i++){
+      
+        
+            Temp_count = 0;
+        }
+            
+                                                   
+        return answer;
+    }
+}
